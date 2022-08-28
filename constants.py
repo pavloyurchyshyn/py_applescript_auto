3c79fcc01ca0d1e723f1419cbfbb93c3da27939a
pavloyurchyshyn <payurchyshyn@gmail.com>
2022-08-28 15:54:51 +0300

    basis

---
+++ constants.py
@@ -0,0 +1,30 @@
+class AppleScrConst:
+    AppleScrTrue = 'true'
+    AppleScrFalse = 'false'
+    MissValue = 'missing value'
+    SysEvents = 'System Events'
+
+    class ObjProperty:
+        Name = 'name'
+        Title = 'title'
+        Class = 'class'
+
+        Enabled = 'enabled'
+        Selected = 'selected'
+        Focused = 'focused'
+
+        Position = 'position'
+        Size = 'size'
+        Orientation = 'orientation'
+
+        Help = 'help'
+        Value = 'value'
+        MaximumValue = 'maximum value'
+        MinimumValue = 'minimum value'
+
+        EntireContents = 'entire contents'
+        Role = 'role'
+        Subrole = 'subrole'
+        RoleDescription = 'role description'
+
+    ObjProp = ObjProperty
--- script.py
+++ script.py
@@ -1,79 +1,132 @@
-class ArgsKeys:
-    Script = '@script'
-    Scr = Script
-    Window = '@window'
-    Win = Window
-    Application = '@application'
-    App = Application
-    Value = '@value'
+from common_scripts import ArgsKeys, CommonScripts
+from constants import AppleScrConst
 
 
-class AppleScrConst:
-    SysEvents = 'System Events'
-
-
-class CommonScripts:
-    TellApplication = f'tell application "{ArgsKeys.App}"\n{ArgsKeys.Scr}\nend tell\n'
-    TellApp = TellApplication
-    Delay = f'delay {ArgsKeys.Value}\n'
-    Click = f'click {ArgsKeys.Value}\n'
-    ClickAt = f'click at {ArgsKeys.Value}\n'
-
-    @staticmethod
-    def get_tell_app(app: str) -> str:
-        return CommonScripts.TellApp.replace(ArgsKeys.App, app, 1)
-
-    tell_app = get_tell_app
-
-    @staticmethod
-    def get_delay(delay) -> str:
-        return CommonScripts.Delay.replace(ArgsKeys.Value, delay)
-
-    delay = get_delay
-
-    @staticmethod
-    def get_click(obj):
-        return CommonScripts.Click.replace(ArgsKeys.Value, obj)
-
-    @staticmethod
-    def get_click_at(position: tuple) -> str:
-        pos = '{p}'.replace('p', ', '.join(map(str, position[:2])))
-        return CommonScripts.ClickAt.replace(ArgsKeys.Value, pos)
-
-
-class AScript:
+class BaseAScript:
     def __init__(self, body: str = ArgsKeys.Script):
         self.body = str(body)
 
-    def add(self, script, key=ArgsKeys.Script, pos=0, n_arg=None, delay=0):
-        if delay:
-            script = f'{script}\ndelay {delay}'
-        if n_arg:
-            script = f'{script}\n{n_arg}'
-        self.body = self.replace_to_position(self.body, script, key, pos)
+    def add_script(self, script, **kwargs):
+        return self.add(script, key=ArgsKeys.Script, **kwargs)
 
+    def add_value(self, script, **kwargs):
+        return self.add(script, key=ArgsKeys.Value, **kwargs)
+
+    def add(self, script, key: str = ArgsKeys.Script, pos: int = 0,
+            next_key: str = None, next_key_delim='\n',
+            delay: int = 0):
+        """
+        Main method of script creation.
+        :param script: str or AScript object
+        :param key: a key which will be replaced by script
+        :param pos: which key replace in case of a few same keys in the body
+        :param next_key: add key after script
+        :param next_key_delim: symbol between script and new key
+        :param delay: delay after script. Note: no output returns after a delay.
+        :return: AScript object
+        """
+        script = str(script)
+        if delay:
+            script = f'{script}delay {delay}'
+        if next_key:
+            script = f'{script}{next_key_delim}{next_key}'
+        self.body = self.replace_to_position(self.body, script, key, pos)
+        return self
+
+    def insert_scripts(self, *scripts, key: str = ArgsKeys.Script, pos: int = 0, **kwargs):
+        """
+        Place few scripts in some position.
+        :param scripts: the iterable object which contains str or AScript objects
+        """
+        self.add('\n'.join(map(str, scripts)), key=key, pos=pos, **kwargs)
         return self
 
     def add_kwargs(self, kwargs: dict):
+        """
+        Replacing keys by values.
+        :type kwargs: dict
+        :return:
+        """
         for k, v in kwargs.items():
             self.body.replace(k, v, 1)
 
         return self
 
-    def add_delay(self, delay=5, n_arg=None):
-        self.add('', delay=delay, n_arg=n_arg)
+    def add_delay(self, delay=5, next_key=None):
+        """
+        Add delay in applescript, equal to sleep in python.
+        :param delay: seconds
+        :type delay: int
+        """
+        self.add('', delay=delay, next_key=next_key)
         return self
 
     @staticmethod
-    def replace_to_position(body, script, key=ArgsKeys.Script, position=0):
+    def replace_to_position(body, script, key=ArgsKeys.Script, position=0) -> str:
         body: list = body.split(key)
-        body[position] = f'{body[position]}{script}{body[position + 1]}'
-        body.pop(position + 1)
+        body[position] = f'{body[position]}{script}{body.pop(position + 1)}'
         return key.join(body)
 
-    def click(self, obj, key=ArgsKeys.Script, pos=0, n_arg=None, delay=0):
-        self.add(CommonScripts.get_click(obj), key=key, pos=pos, n_arg=n_arg, delay=delay)
-        return self
+    def join_scripts(self, *scripts):
+        """
+        Add scripts in the end.
+        """
+        self.body = '\n'.join((self.body, *map(str, scripts)))
+
+
+class AScript(BaseAScript):
+
+    # todo, get screen of element
+
+    def get_obj_size(self, obj, **kwargs):
+        return self.property_of(AppleScrConst.ObjProp.Size, obj=obj, **kwargs)
+
+    def get_obj_pos(self, obj, **kwargs):
+        return self.property_of(AppleScrConst.ObjProp.Position, obj=obj, **kwargs)
+
+    def property_of(self, prop: str, obj: str, **kwargs):
+        return self.add(f'{prop} of {obj}', **kwargs)
+
+    def do_screen_of_area(self, file: str, rect: tuple, **kwargs):
+        """
+        :param file: where to save the screen capture, 1 file per screen.
+        :param rect: -R x,y,w,h Capture a screen rectangle, top,left,width,height.
+        """
+        return self.do_screen(file, options=f'-R {",".join(map(str, rect[:4]))}', **kwargs)
+
+    def get_properties_of(self, object_, **kwargs):
+        return self.add(CommonScripts.properties_of(object_), **kwargs)
+
+    def do_screen(self, file: str, options: str, **kwargs):
+        """
+        https://ss64.com/osx/screencapture.html
+        """
+        return self.do_shell_script(CommonScripts.screencapture(file, options), **kwargs)
+
+    def do_shell_script(self, script: str or tuple, **kwargs):
+        script = script if type(script) is str else ' '.join(script)
+        return self.add(CommonScripts.do_shell_script(script), **kwargs)
+
+    def keystroke(self, value, **kwargs):
+        return self.add(CommonScripts.keystroke(value), **kwargs)
+
+    def set_variable(self, name: str, **kwargs):
+        return self.add(CommonScripts.set_var(name), **kwargs)
+
+    def tell_system_events(self, **kwargs):
+        return self.add(CommonScripts.tell_app(AppleScrConst.SysEvents), **kwargs)
+
+    def repeat_n_times(self, times: int, **kwargs):
+        return self.add(CommonScripts.repeat(str(times)), **kwargs)
+
+    def click(self, obj, key=ArgsKeys.Script, pos=0, next_key=None, delay=0):
+        return self.add(CommonScripts.click(obj), key=key, pos=pos, next_key=next_key, delay=delay)
+
+    def click_at(self, position, **kwargs):
+        return self.add(CommonScripts.click_at(position), **kwargs)
+
+    def exists(self, obj, **kwargs):
+        return self.add(CommonScripts.exists(obj), **kwargs)
 
     def __str__(self):
         return self.body
@@ -81,6 +134,10 @@ class AScript:
 
 if __name__ == '__main__':
     ascript = AScript()
-    ascript.add_delay(n_arg=ArgsKeys.Script)
-    ascript
+    ascript.tell_system_events()
+    ascript.click_at((100, 100), next_key=ArgsKeys.Script)
+    ascript.repeat_n_times(10, next_key=ArgsKeys.Script)
+    ascript.add_delay(5, next_key=ArgsKeys.Script)
+    ascript.set_variable('test_var').add('1', key=ArgsKeys.Value)
+    ascript.do_screen_of_area('test.png', (1, 2, 3, 4, 5, 6, 7, 8))
     print(ascript)
